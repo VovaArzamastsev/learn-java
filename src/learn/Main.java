4cb284b1bb87aedc5f9855e38a4948f1a932237f
VovaArzamastsev <goodjob3110@gmail.com>
2022-04-08 20:12:10 +0300

    commit

--- src/learn/Main.java
+++
@@ -1,8 +0,0 @@
-package learn;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/learn/task1/Task1.java
@@ -0,0 +1,19 @@
+package learn.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int num = 3;
+        System.out.println("number = " + num);
+        //yakwo chislo 1 to pushemo 10
+        if (num == 1) {
+            System.out.println(10);
+        }
+        //yakwo chislo 2 to pushemo 20
+        if (num == 2) {
+            System.out.println(20);
+        }
+        if (num == 3) {
+            System.out.println(30);
+        }
+    }
+}
