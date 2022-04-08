76b314ae595da93de9b080cbb9f32362ea0e6eca
Oleksandr Arzamastsev <oleksandr.arzamastsev@auto1.com>
2022-04-08 20:39:16 +0300

    task10: definition

---
+++ src/learn/task10/Task10.java
@@ -0,0 +1,17 @@
+package learn.task10;
+
+public class Task10 {
+
+    /**
+     * Write a program that:
+     * 1. asks the user to enter a number that will define the size of array of names
+     * 2. after that the program asks you to enter names - you will put them into the array
+     * 3. once all the input is done - display all the names that you have recorded.
+     *
+     * Hint: use the 'for' cycle
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
