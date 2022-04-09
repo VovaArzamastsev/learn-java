f76bea307b6554517160b90012869ca7a74fa67a
VovaArzamastsev <goodjob3110@gmail.com>
2022-04-10 00:26:43 +0300

    task11: solution

--- src/learn/task11/Task11.java
+++ src/learn/task11/Task11.java
@@ -1,5 +1,9 @@
 package learn.task11;
 
+import learn.util.ScannerUtil;
+
+import java.util.Scanner;
+
 public class Task11 {
 
     /**
@@ -10,12 +14,37 @@ public class Task11 {
      * 4. In the loop it asks the user to enter a number and puts it into the array of numbers
      * 5. If the user entered 100 (code number), the program prints all numbers and finishes
      * 6. If the array is full and the user enters the number that is not 100 (code number) - write an error message that array is full
-     *
+     * <p>
      * Hint: to solve the task you need to create an additional 'int index = 0' before the loop,
      * and update it inside the loop when the number is added into the array, this 'index' helps you understand if there is still free space in the array
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter size of array: ");
+        int size = ScannerUtil.nextInt(scanner);
+
+        int[] numbers = new int[size];
+
+        int index = 0;
+        boolean flag = true;
+
+        while (flag) {
+            System.out.println("Enter number: ");
+            int numb = ScannerUtil.nextInt(scanner);
+            if (numb == 100) {
+                flag = false;
+                for (int i = 0; i < size; i++) {
+                    System.out.println("Numbers[" + i + "] = " + numbers[i]);
+                }
+            } else {
+                if (index == size) {
+                    System.err.println("Array is full");
+                } else {
+                    numbers[index] = numb;
+                    index++;
+                }
+            }
+        }
     }
-
 }
