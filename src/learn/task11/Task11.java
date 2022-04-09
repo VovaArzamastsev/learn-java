4b584d971485c5acf7d6975ef13bec9d27788f22
OleksandrArzamastsev <oleksandr.arzamastsev@auto1.com>
2022-04-09 23:01:22 +0300

    add new task definitions

---
+++ src/learn/task11/Task11.java
@@ -0,0 +1,21 @@
+package learn.task11;
+
+public class Task11 {
+
+    /**
+     * Write a program that:
+     * 1. Asks the user to enter the size of array of numbers
+     * 2. Creates the array of numbers with the size from step 1
+     * 3. Enters an endless while loop
+     * 4. In the loop it asks the user to enter a number and puts it into the array of numbers
+     * 5. If the user entered 100 (code number), the program prints all numbers and finishes
+     * 6. If the array is full and the user enters the number that is not 100 (code number) - write an error message that array is full
+     *
+     * Hint: to solve the task you need to create an additional 'int index = 0' before the loop,
+     * and update it inside the loop when the number is added into the array, this 'index' helps you understand if there is still free space in the array
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/learn/task15/Task15.java
@@ -0,0 +1,14 @@
+package learn.task15;
+
+public class Task15 {
+
+    /**
+     * Write a program that:
+     * 1.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
